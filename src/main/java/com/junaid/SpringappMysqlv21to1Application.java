1e6225656b31d9185fc69a9d5d704438af3d16b3
junaid malm <24297521+junaid-malim@users.noreply.github.com>
2022-01-11 01:49:47 +0530

    initial commit

---
+++ src/main/java/com/junaid/SpringappMysqlv21to1Application.java
@@ -0,0 +1,13 @@
+package com.junaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappMysqlv21to1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappMysqlv21to1Application.class, args);
+	}
+
+}
