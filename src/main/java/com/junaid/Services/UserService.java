b0e7a7813a3cbd35519d416a4ad073c3111027b4
junaid malm <24297521+junaid-malim@users.noreply.github.com>
2022-01-11 05:26:33 +0530

    all working except put
    workes with id in data and not with id in url

---
+++ src/main/java/com/junaid/Services/UserService.java
@@ -0,0 +1,19 @@
+package com.junaid.Services;
+
+import java.util.List;
+
+import com.junaid.Models.UserModel;
+
+public interface UserService {
+
+	public List<UserModel> getUser();
+
+	public List<UserModel> setUser(UserModel um);
+
+	public List<UserModel> removeUser(long i);
+
+	public UserModel getUserbyId(long parseLong);
+
+	public UserModel putUser(long userid, UserModel um);
+
+}
